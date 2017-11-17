5554fa2a438fe00c05728b1771da55cca86b3c16
ohjaeho <ovsoce@gmail.com>
2017-11-18 08:01:40 +0900

    Create LayoutUtility..

---
+++ app/src/main/java/kr/co/e1/workreport/framework/LayoutUtility.java
@@ -0,0 +1,29 @@
+package kr.co.e1.workreport.framework;
+
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.support.annotation.ColorRes;
+import android.support.annotation.DrawableRes;
+import android.support.v4.content.ContextCompat;
+import android.util.TypedValue;
+
+/**
+ * Created by jaeho on 2017. 11. 18
+ */
+
+public class LayoutUtility {
+
+  public static @DrawableRes int getSelectableItemBackground(Context context) {
+    TypedValue outValue = new TypedValue();
+    context.getTheme().resolveAttribute(android.R.attr.selectableItemBackground, outValue, true);
+    return outValue.resourceId;
+  }
+
+  public static ColorStateList getColorStateList(Context context, @ColorRes int resId) {
+    return ColorStateList.valueOf(ContextCompat.getColor(context, resId));
+  }
+
+  public static int getColor(Context context, @ColorRes int resId) {
+    return ContextCompat.getColor(context, resId);
+  }
+}
